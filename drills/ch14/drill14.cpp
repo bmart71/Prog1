767eab2fc41e7193a9fdc37fddb3b565588e6db7
VoultBoi <71928895+bmart71@users.noreply.github.com>
2022-03-10 14:36:37 +0100

    Add files via upload

---
+++ drills/ch14/drill14.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void slice(){ cout << "---------------------------------------\n"; }
+
+class B1 {
+public:
+	virtual void vf(){ cout << "B1::vf()" << endl; }
+	void f() { cout << "B1::f()" << endl; }
+	virtual void pvf() = 0;
+};
+
+class D1: public B1 {
+public:
+	void vf(){ cout << "D1::vf()" << endl; }
+	void f(){ cout << "D1::f()" << endl; }
+};
+
+class D2: public D1 {
+public:
+	void pvf() { cout << "D2::pfv()" << endl; }
+};
+
+class B2 {
+public:
+	virtual void pvf() = 0;
+};
+
+class D21 : public B2 {
+public:
+	string s;
+	void pvf(){ cout << s << endl; }
+};
+
+class D22 : public B2 {
+public:
+	int n;
+	void pvf(){ cout << n << endl; }
+};
+
+void f(B2& b2ref) {
+	b2ref.pvf();
+}
+
+int main(){
+	try{
+		/*B1 b1;
+		b1.vf();
+		b1.f();
+		
+		slice();
+		
+		D1 d1;
+		d1.vf();
+		d1.f();
+		
+		slice();
+		
+		B1& bref = d1;
+		
+		bref.vf();
+		bref.f();*/
+		
+		D2 d2;
+		d2.f();
+		d2.vf();
+		d2.pvf();
+		
+		slice();
+		
+		D21 d21;
+		d21.s = "d21.s";
+		d21.pvf();
+		
+		D22 d22;
+		d22.n = 22;
+		d22.pvf();
+		
+		slice();
+		
+		f(d21);
+		f(d22);
+	} catch (exception &e) {
+		cerr << "error: " << e.what() << '\n';
+		return 1;
+	} catch (...) {
+		cerr << "unknown exception" << '\n';
+		return 2;
+	}
+}
